7036e9957537906130e4f923427a997ae28c95bc
Lattuse <sultik280805@gmail.com>
2025-10-15 18:56:04 +0500

    validation + reg

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-document.getElementById("year").textContent = new Date().getFullYear();
+(function () {
+  "use strict";
+
+  const forms = document.querySelectorAll("form");
+
+  forms.forEach((form) => {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (form.id === "registrationForm") {
+          const password = form.querySelector("#password").value;
+          const confirmPassword = form.querySelector("#confirmPassword").value;
+          if (password !== confirmPassword) {
+            form
+              .querySelector("#confirmPassword")
+              .setCustomValidity("Passwords must match.");
+          } else {
+            form.querySelector("#confirmPassword").setCustomValidity("");
+          }
+        }
+
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
